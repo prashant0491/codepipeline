a6153c370e6697b2bc09bfa5f6880dce6fee36df
prashant <prashantjadhav4129@gmail.com>
2025-01-17 17:36:51 +0530

    lkl

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
  res.send('Welcome  to mumbai city ');
 >>>>>>> b54c9f4a4c698237f4b2947a88fa34bc4b32faff
 =======
- res.send('Welcome to pune kothurd');
+ res.send('Welcome to latur golai');
 >>>>>>> 2d820399800124f13982c07dce6c03ea744bc320
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
