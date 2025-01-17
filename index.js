7e09fe5a74e71203761acb8df6b94b873d31a9cf
prashant <prashantjadhav4129@gmail.com>
2025-01-17 15:43:59 +0530

    latur

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
 <<<<<<< HEAD
- res.send('Welcome to latur to pune to mumbai');
+ res.send('Welcome to latur ');
 =======
  res.send('Welcome to pune kothurd');
 >>>>>>> 2d820399800124f13982c07dce6c03ea744bc320
