35a8388a36417c019e33d4e6ae904376a0b53d82
prashant <prashantjadhav4129@gmail.com>
2025-01-17 18:01:58 +0530

    fortune

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-<<<<<<< HEAD
-<<<<<<< HEAD
- res.send('Welcome to latur ');
-=======
- res.send('Welcome  to mumbai city ');
->>>>>>> b54c9f4a4c698237f4b2947a88fa34bc4b32faff
+ res.send('hello fortune cloud ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
